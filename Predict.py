140252bd1581e6a2ce979b548c1d9b942540d251
Eric R Boland <eric.boland@uvm.edu>
2019-09-18 13:33:48 -0400

    Implemented kNN algorithm for data set

--- Predict.py
+++ Predict.py
@@ -1,5 +1,6 @@
 from knn import KNN
 import matplotlib.pyplot as plt
+import numpy as np
 
 knn = KNN()
 
@@ -8,9 +9,29 @@ knn.Load_Dataset('iris.csv')
 x = knn.data[:, 0]
 y = knn.data[:, 1]
 
+trainX = knn.data[::2, 0:2]
+trainy = knn.target[::2]
+
+testX = knn.data[1::2, 0:2]
+testy = knn.target[1::2]
+
+knn.Use_K_Of(15)
+knn.Fit(trainX, trainy)
+for i in range(0, len(testX)):
+    actualClass = testy[i]
+    prediction = knn.Predict(testX[i, 0:2])
+    print(actualClass, prediction)
+
+colors = np.zeros((3, 3), dtype='f')
+colors[0, :] = [1, 0.5, 0.5]
+colors[1, :] = [0.5, 1, 0.5]
+colors[2, :] = [0.5, 0.5, 1]
 
 plt.figure()
-
-plt.scatter(x, y, c=knn.target)
+[numItems, numFeatures] = knn.data.shape
+for i in range(0, numItems / 2):
+    itemClass = int(trainy[i])
+    currColor = colors[itemClass, :]
+    plt.scatter(trainX[i, 0], trainX[i, 1], facecolor=currColor, lw=2)
 
 plt.show()
