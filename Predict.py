6a8822ad7c3a78a9a8dbb22b3b24907f9197de99
Eric R Boland <eric.boland@uvm.edu>
2019-09-18 12:52:43 -0400

    Created scatter plot for iris data set

--- Predict.py
+++ Predict.py
@@ -1,1 +1,16 @@
-print 1
+from knn import KNN
+import matplotlib.pyplot as plt
+
+knn = KNN()
+
+knn.Load_Dataset('iris.csv')
+
+x = knn.data[:, 0]
+y = knn.data[:, 1]
+
+
+plt.figure()
+
+plt.scatter(x, y, c=knn.target)
+
+plt.show()
