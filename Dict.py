e59849b9a07e50ac3b10195cf68c9b08611f5417
erboland12 <52169114+erboland12@users.noreply.github.com>
2019-10-15 15:28:02 -0400

    Update Dict.py

--- Dict.py
+++ Dict.py
@@ -4,9 +4,14 @@ database = pickle.load(open('userData/database.p','rb'))
 
 userName = raw_input('Please enter your name: ')
 if userName in database:
+    logs = database[userName]['logins']
+    temp = logs + 1
+    database[userName]['logins'] = temp
+    
     print 'welcome back ' + userName + '.'
 else:
     database[userName] = {}
+    database[userName]['logins'] = 1
     print 'welcome ' + userName + '.'
 
 pickle.dump(database,open('userData/database.p','wb'))
